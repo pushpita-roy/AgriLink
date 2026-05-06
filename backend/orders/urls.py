5168fb0599ad817b9346c7556cb07bead174e310
pushpita-roy <20234103326@cse.bubt.edu.bd>
2026-05-06 14:16:50 +0600

    order status

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.OrderListView.as_view(), name='order-list'),
     path('place/', views.place_order_view, name='place-order'),
-    path('stats/', views.order_stats_view, name='order-stats'), # <--- ADD THIS
+    path('stats/', views.order_stats_view, name='order-stats'), 
     path('<int:pk>/status/', views.update_order_status_view, name='update-order-status'),
 ]
