b2862218a5f791bb01705fa15f4a7bf6eed159cf
pushpita-roy <20234103326@cse.bubt.edu.bd>
2026-05-06 13:04:12 +0600

    product add

--- backend/agrilink/urls.py
+++ backend/agrilink/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # Added re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse # Import this
+from django.http import HttpResponse
+from django.views.static import serve # Added this
 
-# Add this small function
 def home(request):
     return HttpResponse("<h1>AgriLink Backend is Live!</h1>")
 
 urlpatterns = [
-    path('', home), # Add this line for the homepage
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/products/', include('products.urls')),
@@ -17,6 +17,11 @@ urlpatterns = [
     path('api/cart/', include('cart.urls')),
 ]
 
-# This is correct - it keeps media working in local dev
+# 1. Keep local dev working
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# 2. FIX: Force Render to serve media files
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
