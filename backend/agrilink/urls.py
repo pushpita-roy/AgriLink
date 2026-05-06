67abc1db01a51596a6a0fc8ed49f1403f1a7839d
pushpita-roy <20234103326@cse.bubt.edu.bd>
2026-05-06 13:06:38 +0600

    product add

--- backend/agrilink/urls.py
+++ backend/agrilink/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path # Added re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-from django.views.static import serve # Added this
+from django.views.static import serve # Added serve
 
 def home(request):
     return HttpResponse("<h1>AgriLink Backend is Live!</h1>")
@@ -17,11 +17,11 @@ urlpatterns = [
     path('api/cart/', include('cart.urls')),
 ]
 
-# 1. Keep local dev working
+# Keep your debug setting
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# 2. FIX: Force Render to serve media files
+# Add this for Render to fetch images in production
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
